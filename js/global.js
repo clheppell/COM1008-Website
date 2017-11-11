3d1e04028de85c91cbc9225f6ed61ebf7be9aaae
Robinator247 <robbie.thandi@gmail.com>
2017-11-11 14:44:57 +0000

    Added a show/hide button for the menu. Still needs to be styled properly but the functionality is there. Added .gitignore so files from my IDE don't get committed

--- js/global.js
+++ js/global.js
@@ -3,3 +3,22 @@
     Chris Heppell & Robbie Thandi
     Copyright 2017
 */
+
+function showHide(visibleDisplayValue, target){
+    // Function that takes the display value it should have and will toggle them
+    if(target.style.display === "none"){
+        target.style.display = visibleDisplayValue;
+    }
+    else {
+        target.style.display = "none";
+    }
+}
+
+var showHideNavButton = document.getElementById('showHideNavButton');
+var nav = document.getElementById('menuBar');
+
+// Event listener on the menu button to detect click
+showHideNavButton.addEventListener('click', function () {
+    // Calls the showHide function. Has to be a function calling a function so that the display can be passed to showHide
+    showHide("block", nav)
+}, false);
