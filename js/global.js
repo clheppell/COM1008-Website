5917027f98c54e39a8ec826001e2a19f947758e5
Chris Heppell <clheppell1@sheffield.ac.uk>
2017-11-16 13:49:44 +0000

    New font, DIA facilities page, new CSS style
    Switches the font to Open Sans. System fonts looked too corporate.
    
    Adds a page for stuff about Diamond Facilities.
    
    Ensures menu is hidden by default on mobile and you choose to open it.
    
    Some new but unfinished table styles and also adds additional form styles to override webkit defaults.

--- js/global.js
+++ js/global.js
@@ -6,11 +6,11 @@
 
 function showHide(visibleDisplayValue, target){
     // Function that takes the display value it should have and will toggle them
-    if(target.style.display === "none"){
+    if(target.style.display === "block"){
         target.style.display = visibleDisplayValue;
     }
     else {
-        target.style.display = "none";
+        target.style.display = "block";
     }
 }
 
@@ -20,5 +20,5 @@ var nav = document.getElementById('menuBar');
 // Event listener on the menu button to detect click
 showHideNavButton.addEventListener('click', function () {
     // Calls the showHide function. Has to be a function calling a function so that the display can be passed to showHide
-    showHide("block", nav)
+    showHide("none", nav)
 }, false);
